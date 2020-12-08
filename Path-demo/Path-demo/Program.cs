5c0f2472b9a9e812af9feb29cf97f3145a01e2b5
Abrão Astério Junior <abcorinthians@gmail.com>
2020-12-08 20:43:06 -0300

    Demo Path

---
+++ Path-demo/Path-demo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace Path_demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"E:\Advanced-oop\Path-demo\myfolder\file1.txt";
+
+            Console.WriteLine("DirectorySeparatorChar: " + Path.DirectorySeparatorChar); //OUTPUT: \
+
+            Console.WriteLine("PathSeparator: " + Path.PathSeparator); //OUTPUT: ;
+
+            Console.WriteLine("GetDirectoryName: " + Path.GetDirectoryName(path)); //OUTPUT: E:\Advanced-oop\Path-demo\myfolder
+
+            Console.WriteLine("GetFileName: " + Path.GetFileName(path)); //OUTPUT: file1.txt
+
+            Console.WriteLine("GetExtension: " + Path.GetExtension(path)); //OUTPUT: .txt
+
+            Console.WriteLine("GetFileNameWithoutExtension: " + Path.GetFileNameWithoutExtension(path)); //OUTPUT: file1
+
+            Console.WriteLine("GetFullPath: " + Path.GetFullPath(path)); //OUTPUT:  E:\Advanced-oop\Path-demo\myfolder\file1.txt
+
+            Console.WriteLine("GetTempPath: " + Path.GetTempPath()); //OUTPUT: Will show the path of your temporary folder from disc C
+        }
+    }
+}
