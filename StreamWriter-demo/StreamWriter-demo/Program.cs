6a3b5e274e761f762c2ea230cd575278268d4570
Abrão Astério Junior <abcorinthians@gmail.com>
2020-12-08 20:04:20 -0300

    Demo StreamWriter

---
+++ StreamWriter-demo/StreamWriter-demo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace StreamWriter_demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //The program will read the lines of the original file and pass the content of this file to another , with the letters in capitals.
+
+            string sourcePath = @"E:\Advanced-oop\StreamWriter-demo\file1.txt";
+            string targetPath = @"E:\Advanced-oop\StreamWriter-demo\file2.txt";
+            try
+            {
+                string[] lines = File.ReadAllLines(sourcePath);
+                using (StreamWriter sw = File.AppendText(targetPath))
+                {
+                    foreach (string line in lines)
+                    {
+                        sw.WriteLine(line.ToUpper());
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
