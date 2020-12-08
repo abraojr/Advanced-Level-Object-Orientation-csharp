ec75d769acee49dd6ea919aed132c344f0663b65
Abrão Astério Junior <abcorinthians@gmail.com>
2020-12-08 19:46:38 -0300

    Demo 2 Using block

---
+++ Using-block-02/Using-block-02/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace Using_block_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"E:\Advanced-oop\Using-block-02\file1.txt";
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
