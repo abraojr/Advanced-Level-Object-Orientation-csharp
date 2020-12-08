cfa57c0a3b48dd70bdc1a5e3fd5ab58a68ddf794
Abrão Astério Junior <abcorinthians@gmail.com>
2020-12-08 18:26:33 -0300

    Demo 2 FileStream and StreamReader

---
+++ FileStream-and-StreamReader-02/FileStream-and-StreamReader-02/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace FileStream_and_StreamReader_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"E:\Advanced-oop\FileStream-and-StreamReader-02\file1.txt";
+
+            StreamReader sr = null;
+            try
+            {
+                sr = File.OpenText(path);
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    Console.WriteLine(line);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (sr != null) sr.Close();
+            }
+        }
+    }
+}
