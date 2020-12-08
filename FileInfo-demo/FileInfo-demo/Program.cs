99917edc684553a1ac78b65256c7cfa323e425de
Abrão Astério Junior <abcorinthians@gmail.com>
2020-12-08 18:03:09 -0300

    Demo FileInfo

---
+++ FileInfo-demo/FileInfo-demo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace FileInfo_demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //A text file must be created beforehand , for the program to read this file and make a copy to another file
+
+            string sourcePath = @"E:\Advanced-oop\FileInfo-demo\file1.txt"; //original file path
+            string targetPath = @"E:\Advanced-oop\FileInfo-demo\file2.txt"; // path of the original file copy
+
+            try
+            {
+                FileInfo fileInfo = new FileInfo(sourcePath);
+                fileInfo.CopyTo(targetPath);
+                string[] lines = File.ReadAllLines(sourcePath);
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
