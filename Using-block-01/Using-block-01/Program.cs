0e987f193842b17940267d5003591fa8f189abd3
Abrão Astério Junior <abcorinthians@gmail.com>
2020-12-08 18:46:43 -0300

    Demo 1 Using block

---
+++ Using-block-01/Using-block-01/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace Using_block_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* The block using is a simplified syntax that ensures that the IDisposable objects will be closed.
+             * IDisposable objects are NOT managed by the CLR. They need to be manually closed. Examples: Font, FileStream, StreamReader, StreamWriter.*/
+
+            string path = @"E:\Advanced-oop\Using-block-01\file1.txt";
+            try
+            {
+                using (FileStream fs = new FileStream(path, FileMode.Open))
+                {
+                    using (StreamReader sr = new StreamReader(fs))
+                    {
+                        string line = sr.ReadLine();
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
