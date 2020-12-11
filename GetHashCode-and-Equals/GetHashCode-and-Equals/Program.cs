a9ecc8b1938834c0768ed8a06ecaa6bf0e4916c2
Abrão Astério Junior <abcorinthians@gmail.com>
2020-12-11 20:05:02 -0300

    Demo GetHashCode and Equals

---
+++ GetHashCode-and-Equals/GetHashCode-and-Equals/Program.cs
@@ -0,0 +1,19 @@
+﻿using GetHashCode_and_Equals.Entities;
+using System;
+
+namespace GetHashCode_and_Equals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client a = new Client { Name = "Maria", Email = "maria@gmail.com" };
+            Client b = new Client { Name = "Alex", Email = "maria@gmail.com" };
+
+            Console.WriteLine(a.Equals(b)); //OUTPUT: True
+            Console.WriteLine(a == b); //OUTPUT: False
+            Console.WriteLine(a.GetHashCode()); //OUTPUT: -257079879
+            Console.WriteLine(b.GetHashCode()); //OUTPUT: -257079879
+        }
+    }
+}
