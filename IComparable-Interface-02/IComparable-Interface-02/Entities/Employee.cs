d972ec49a803a8dac2787154500185a1c8745419
Abrão Astério Junior <abcorinthians@gmail.com>
2020-12-11 02:03:27 -0300

    Demo 2 IComparable Interface: List of employees ordered by salary

---
+++ IComparable-Interface-02/IComparable-Interface-02/Entities/Employee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace IComparable_Interface_02.Entities
+{
+    class Employee : IComparable
+    {
+        public string Name { get; set; }
+        public double Salary { get; set; }
+
+        public Employee(string employeeCsv)
+        {
+            string[] vect = employeeCsv.Split(",");
+            Name = vect[0];
+            Salary = double.Parse(vect[1], CultureInfo.InvariantCulture);
+        }
+
+        public override string ToString()
+        {
+            return Name + ", U$ " + Salary.ToString("F2", CultureInfo.InvariantCulture);
+        }
+        public int CompareTo(object obj)
+        {
+            if (!(obj is Employee))
+            {
+                throw new ArgumentException("Comparing error: argument is not an employee");
+            }
+            Employee other = obj as Employee;
+            return Salary.CompareTo(other.Salary);
+        }
+    }
+}
---
+++ IComparable-Interface-02/IComparable-Interface-02/Program.cs
@@ -0,0 +1,37 @@
+﻿using IComparable_Interface_02.Entities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace IComparable_Interface_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"E:\Advanced-oop\IComparable-Interface-02\file.txt";
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    List<Employee> list = new List<Employee>();
+                    while (!sr.EndOfStream)
+                    {
+                        list.Add(new Employee(sr.ReadLine()));
+                    }
+                    list.Sort();
+                    foreach (Employee emp in list)
+                    {
+                        Console.WriteLine(emp);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
