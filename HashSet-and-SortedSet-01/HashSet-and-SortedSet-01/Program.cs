58ebe19d259dc65b6d0ea0501c71e92cb38dc556
Abrão Astério Junior <abcorinthians@gmail.com>
2020-12-11 20:12:39 -0300

    Demo 1 HashSet and SortedSet

---
+++ HashSet-and-SortedSet-01/HashSet-and-SortedSet-01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HashSet_and_SortedSet_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> set = new HashSet<string>();
+
+            set.Add("TV");
+            set.Add("Notebook");
+            set.Add("Tablet");
+
+            Console.WriteLine(set.Contains("Notebook"));//OUTPUT:True
+            foreach (String p in set)
+            {
+                Console.WriteLine(p);
+            }
+
+        }
+    }
+}
