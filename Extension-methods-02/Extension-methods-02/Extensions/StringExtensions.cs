1178f5535c4ccf0f4f1000cc5d469f15ccb5962f
Abrão Astério Junior <abcorinthians@gmail.com>
2020-12-14 18:44:05 -0300

    Demo 2 Extension methods

---
+++ Extension-methods-02/Extension-methods-02/Extensions/StringExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace Extension_methods_02.Extensions
+{
+    static class StringExtensions
+    {
+        public static string Cut(this string thisObj, int count)
+        {
+            if (thisObj.Length <= count)
+            {
+                return thisObj;
+            }
+            else
+            {
+                return thisObj.Substring(0, count) + "...";
+            }
+        }
+    }
+}
---
+++ Extension-methods-02/Extension-methods-02/Program.cs
@@ -0,0 +1,17 @@
+﻿using Extension_methods_02.Extensions;
+using System;
+
+namespace Extension_methods_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Let's create an extension method called "Cut(int)" in the String class to receive an integer value as parameter and generate a cutout of the 
+             *original string of that size.*/
+
+            string s1 = "Good morning dear students";
+            Console.WriteLine(s1.Cut(10)); //OUTPUT: Good morni...
+        }
+    }
+}
