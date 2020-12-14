7c4753637293f0dda58dda5e11d897e07ea15964
Abrão Astério Junior <abcorinthians@gmail.com>
2020-12-14 20:21:02 -0300

    Demo 4 Delegates: Action - example with ForEach

---
+++ Delegates-04/Delegates-04/Program.cs
@@ -0,0 +1,27 @@
+﻿using Delegates_04.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace Delegates_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Make a program that, from a list of products, increases the price of the products by 10%.*/
+
+            List<Product> list = new List<Product>();
+            list.Add(new Product("Tv", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.50));
+            list.Add(new Product("HD Case", 80.90));
+
+            list.ForEach(p => { p.Price += p.Price * 0.1; });
+            foreach (Product p in list)
+            {
+                Console.WriteLine(p);
+            }
+        }
+    }
+}
+
