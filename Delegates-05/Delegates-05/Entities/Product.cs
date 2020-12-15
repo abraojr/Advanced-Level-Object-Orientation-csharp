55af8efbc1fe3a2bc05e427294ab575cca9fdf8c
Abrão Astério Junior <abcorinthians@gmail.com>
2020-12-14 21:32:48 -0300

    Demo 5 Delegates: Func - example with Select

---
+++ Delegates-05/Delegates-05/Entities/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Delegates_05.Entities
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+        public override string ToString()
+        {
+            return Name + ", U$ " + Price.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
