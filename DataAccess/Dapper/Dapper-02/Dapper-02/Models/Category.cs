6a48122d2857390a87dc67d076aa0b5fb7ec239b
Abrão Astério Junior <abraoastjr@gmail.com>
2022-06-22 16:41:54 -0300

    INSERT command using Dapper

---
+++ DataAccess/Dapper/Dapper-02/Dapper-02/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dapper_02.Models
+{
+    class Category
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Url { get; set; }
+        public string Summary { get; set; }
+        public int Order { get; set; }
+        public string Description { get; set; }
+        public bool Featured { get; set; }
+    }
+}
---
+++ DataAccess/Dapper/Dapper-02/Dapper-02/Program.cs
@@ -0,0 +1,67 @@
+﻿using Dapper;
+using Dapper_02.Models;
+using Microsoft.Data.SqlClient;
+using System;
+
+namespace Dapper_02
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            const string connectionString = "Data Source=localhost,1433; Database=balta; User ID=sa; Password=SenhaDoSA123; Encrypt=False";
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                ListCategories(connection); // SELECT OPERATION
+                CreateCategory(connection); // INSERT OPERTATION
+            }
+        }
+
+        static void ListCategories(SqlConnection connection)
+        {
+            var categories = connection.Query<Category>("SELECT [Id], Title FROM CATEGORY");
+
+            foreach (var item in categories)
+            {
+                Console.WriteLine($"{item.Id} - {item.Title}");
+            }
+        }
+
+        static void CreateCategory(SqlConnection connection)
+        {
+            var category = new Category();
+            category.Id = Guid.NewGuid();
+            category.Title = "Amazon AWS";
+            category.Url = "amazon";
+            category.Description = "Category for AWS Services";
+            category.Order = 8;
+            category.Summary = "AWS Cloud";
+            category.Featured = false;
+
+            var insertSql = @"INSERT INTO
+                       [Category]
+                   VALUES(
+                          @Id,
+                          @Title,
+                          @Url,
+                          @Description,
+                          @Order,
+                          @Summary,
+                          @Featured)";
+
+            var rows = connection.Execute(insertSql, new
+            {
+                category.Id,
+                category.Title,
+                category.Url,
+                category.Description,
+                category.Order,
+                category.Summary,
+                category.Featured
+            });
+
+            Console.WriteLine($"{rows} rows affected");
+        }
+    }
+}
