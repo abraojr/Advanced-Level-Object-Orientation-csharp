2f2b0c18795add6f0fd739c5c2dc7939786928b3
Abrão Astério Junior <abraoastjr@gmail.com>
2022-06-22 18:58:40 -0300

    DELETE command and SELECT TOP using Dapper

---
+++ DataAccess/Dapper/Dapper-04/Dapper-04/Program.cs
@@ -0,0 +1,46 @@
+﻿using Dapper;
+using Dapper_04.Models;
+using Microsoft.Data.SqlClient;
+using System;
+
+namespace Dapper_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string connectionString = "Data Source=localhost,1433; Database=balta; User ID=sa; Password=SenhaDoSA123; Encrypt=False";
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                GetCategory(connection); // SELECT TOP
+                DeleteCategory(connection); // DELETE OPERATION
+            }
+        }
+
+        static void GetCategory(SqlConnection connection)
+        {
+            var category = connection
+                .QueryFirstOrDefault<Category>(
+                    "SELECT TOP 1 [Id], [Title] FROM [Category] WHERE [Id]=@id",
+                    new
+                    {
+                        id = "45444105-a436-43c7-938d-2b0eb26e95e7"
+                    }
+                );
+
+            Console.WriteLine($"{category.Id} - {category.Title}");
+        }
+
+        static void DeleteCategory(SqlConnection connection)
+        {
+            var deleteQuery = "DELETE FROM [Category] WHERE [Id]=@id";
+            var rows = connection.Execute(deleteQuery, new
+            {
+                id = new Guid("45444105-a436-43c7-938d-2b0eb26e95e7"),
+            });
+
+            Console.WriteLine($"{rows} rows affected");
+        }
+    }
+}
