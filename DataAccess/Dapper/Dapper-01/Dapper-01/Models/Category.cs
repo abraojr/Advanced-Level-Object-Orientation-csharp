ce8d01b4ff12e36abc34620fa8a560ec7345d862
Abrão Astério Junior <abraoastjr@gmail.com>
2022-06-22 14:41:03 -0300

    Introduction to Dapper

---
+++ DataAccess/Dapper/Dapper-01/Dapper-01/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dapper_01.Models
+{
+    public class Category
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+    }
+}
---
+++ DataAccess/Dapper/Dapper-01/Dapper-01/Program.cs
@@ -0,0 +1,26 @@
+﻿using Dapper;
+using Dapper_01.Models;
+using Microsoft.Data.SqlClient;
+using System;
+
+namespace Dapper_01
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            const string connectionString = "Data Source=localhost,1433; Database=balta; User ID=sa; Password=SenhaDoSA123; Encrypt=False";
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var categories = connection.Query<Category>("SELECT [Id], Title FROM CATEGORY");
+
+                foreach (var category in categories)
+                {
+                    Console.WriteLine($"{category.Id} - {category.Title}");
+                }
+            }
+
+        }
+    }
+}
