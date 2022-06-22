beddb5dd1965c1a16266cef8ad9fee305d9aa22e
Abrão Astério Junior <abraoastjr@gmail.com>
2022-06-22 17:07:19 -0300

    UPDATE command using Dapper

---
+++ DataAccess/Dapper/Dapper-03/Dapper-03/Program.cs
@@ -0,0 +1,43 @@
+﻿using Dapper;
+using Dapper_03.Models;
+using Microsoft.Data.SqlClient;
+using System;
+
+namespace Dapper_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string connectionString = "Data Source=localhost,1433; Database=balta; User ID=sa; Password=SenhaDoSA123; Encrypt=False";
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                UpdateCategory(connection); // UPDATE OPERATION
+                ListCategories(connection); // SELECT OPERATION
+            }
+        }
+
+        static void ListCategories(SqlConnection connection)
+        {
+            var categories = connection.Query<Category>("SELECT [Id], Title FROM CATEGORY");
+
+            foreach (var item in categories)
+            {
+                Console.WriteLine($"{item.Id} - {item.Title}");
+            }
+        }
+
+        static void UpdateCategory(SqlConnection connection)
+        {
+            var updateQuery = "UPDATE [Category] SET [Title]=@title WHERE [Id]=@id";
+            var rows = connection.Execute(updateQuery, new
+            {
+                id = new Guid("af3407aa-11ae-4621-a2ef-2028b85507c4"),
+                title = "Frontend 2022"
+            });
+
+            Console.WriteLine($"{rows} rows affected");
+        }
+    }
+}
