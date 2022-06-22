2e6726ddf2a6d56e89d40390fa9e2f0c708d4c2b
Abrão Astério Junior <abraoastjr@gmail.com>
2022-06-22 20:23:07 -0300

    Execute Many using Dapper

---
+++ DataAccess/Dapper/Dapper-05/Dapper-05/Program.cs
@@ -0,0 +1,76 @@
+﻿using Dapper;
+using Dapper_05.Models;
+using Microsoft.Data.SqlClient;
+using System;
+
+namespace Dapper_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string connectionString = "Data Source=localhost,1433; Database=balta; User ID=sa; Password=SenhaDoSA123; Encrypt=False";
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                CreateManyCategory(connection);
+            }
+        }
+
+        static void CreateManyCategory(SqlConnection connection)
+        {
+            var category = new Category();
+            category.Id = Guid.NewGuid();
+            category.Title = "Amazon AWS";
+            category.Url = "amazon";
+            category.Description = "Category for AWS Services";
+            category.Order = 8;
+            category.Summary = "AWS Cloud";
+            category.Featured = false;
+
+            var category2 = new Category();
+            category2.Id = Guid.NewGuid();
+            category2.Title = "New Category";
+            category2.Url = "new-category";
+            category2.Description = "New category";
+            category2.Order = 9;
+            category2.Summary = "Category";
+            category2.Featured = true;
+
+            var insertSql = @"INSERT INTO 
+                    [Category] 
+                VALUES(
+                    @Id, 
+                    @Title, 
+                    @Url, 
+                    @Summary, 
+                    @Order, 
+                    @Description, 
+                    @Featured)";
+
+            var rows = connection.Execute(insertSql, new[]{
+                new
+                {
+                    category.Id,
+                    category.Title,
+                    category.Url,
+                    category.Summary,
+                    category.Order,
+                    category.Description,
+                    category.Featured
+                },
+                new
+                {
+                    category2.Id,
+                    category2.Title,
+                    category2.Url,
+                    category2.Summary,
+                    category2.Order,
+                    category2.Description,
+                    category2.Featured
+                }
+            });
+            Console.WriteLine($"{rows} rows affected");
+        }
+    }
+}
