e41041074df8b6b52cb5f1791bb07ed017137158
Abrão Astério Junior <abraoastjr@gmail.com>
2022-06-23 11:20:34 -0300

    Reading Views using Dapper

---
+++ DataAccess/Dapper/Dapper-09/Dapper-09/Program.cs
@@ -0,0 +1,30 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using System;
+
+namespace Dapper_09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string connectionString = "Data Source=localhost,1433; Database=balta; User ID=sa; Password=SenhaDoSA123; Encrypt=False";
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                ReadView(connection);
+            }
+        }
+
+        static void ReadView(SqlConnection connection)
+        {
+            var sql = "SELECT * FROM [vwCourses]";
+            var courses = connection.Query(sql);
+
+            foreach (var item in courses)
+            {
+                Console.WriteLine($"{item.Id} - {item.Title}");
+            }
+        }
+    }
+}
