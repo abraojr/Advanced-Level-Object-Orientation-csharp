de3201faa25e258a943a4abf3be924ae295f41fd
Abrão Astério Junior <abraoastjr@gmail.com>
2022-06-23 10:25:32 -0300

    Running a Reading Procedure using Dapper

---
+++ DataAccess/Dapper/Dapper-07/Dapper-07/Program.cs
@@ -0,0 +1,32 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Data;
+
+namespace Dapper_07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string connectionString = "Data Source=localhost,1433; Database=balta; User ID=sa; Password=SenhaDoSA123; Encrypt=False";
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                ExecuteReadProcedure(connection);
+            }
+        }
+
+        static void ExecuteReadProcedure(SqlConnection connection)
+        {
+            var procedure = "[spGetCoursesByCategory]";
+            var parameters = new { CategoryId = "09ce0b7b-cfca-497b-92c0-3290ad9d5142" };
+            var courses = connection.Query(procedure, parameters, commandType: CommandType.StoredProcedure);
+
+            foreach (var item in courses)
+            {
+                Console.WriteLine(item.Title);
+            }
+        }
+    }
+}
