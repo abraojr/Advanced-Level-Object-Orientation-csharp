7054360e3038fec7e0e48255cc2101247d65b3c5
Abrão Astério Junior <abraoastjr@gmail.com>
2022-06-23 13:00:32 -0300

    Transactions using Dapper

---
+++ DataAccess/Dapper/Dapper-15/Dapper-15/Program.cs
@@ -0,0 +1,63 @@
+﻿using Dapper;
+using Dapper_15.Models;
+using Microsoft.Data.SqlClient;
+using System;
+
+namespace Dapper_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string connectionString = "Data Source=localhost,1433; Database=balta; User ID=sa; Password=SenhaDoSA123; Encrypt=False";
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                Transaction(connection);
+            }
+        }
+
+        static void Transaction(SqlConnection connection)
+        {
+            var category = new Category();
+            category.Id = Guid.NewGuid();
+            category.Title = "Test category";
+            category.Url = "Test";
+            category.Description = "Test transaction";
+            category.Order = 8;
+            category.Summary = "Test";
+            category.Featured = false;
+
+            var insertSql = @"INSERT INTO 
+                    [Category] 
+                VALUES(
+                    @Id, 
+                    @Title, 
+                    @Url, 
+                    @Summary, 
+                    @Order, 
+                    @Description, 
+                    @Featured)";
+
+            connection.Open();
+            using (var transaction = connection.BeginTransaction())
+            {
+                var rows = connection.Execute(insertSql, new
+                {
+                    category.Id,
+                    category.Title,
+                    category.Url,
+                    category.Summary,
+                    category.Order,
+                    category.Description,
+                    category.Featured
+                }, transaction);
+
+                transaction.Commit();
+                //transaction.Rollback();
+
+                Console.WriteLine($"{rows} rows affected");
+            }
+        }
+    }
+}
