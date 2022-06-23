c8d767d6bf62db1e5ee6eafc24c00b8cf6019622
Abrão Astério Junior <abraoastjr@gmail.com>
2022-06-23 12:40:46 -0300

    LIKE command using Dapper

---
+++ DataAccess/Dapper/Dapper-14/Dapper-14/Models/Course.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dapper_14.Models
+{
+    class Course
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+    }
+}
---
+++ DataAccess/Dapper/Dapper-14/Dapper-14/Program.cs
@@ -0,0 +1,35 @@
+﻿using Dapper;
+using Dapper_14.Models;
+using Microsoft.Data.SqlClient;
+using System;
+
+namespace Dapper_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string connectionString = "Data Source=localhost,1433; Database=balta; User ID=sa; Password=SenhaDoSA123; Encrypt=False";
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                Like(connection, "api");
+            }
+        }
+
+        static void Like(SqlConnection connection, string term)
+        {
+            var query = @"SELECT * FROM [Course] WHERE [Title] LIKE @exp";
+
+            var items = connection.Query<Course>(query, new
+            {
+                exp = $"%{term}%"
+            });
+
+            foreach (var item in items)
+            {
+                Console.WriteLine(item.Title);
+            }
+        }
+    }
+}
