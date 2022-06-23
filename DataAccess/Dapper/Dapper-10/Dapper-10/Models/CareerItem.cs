2c5999220e1b6cda8a0862ef18b6ab3ebcac62cf
Abrão Astério Junior <abraoastjr@gmail.com>
2022-06-23 11:52:24 -0300

    Implementing One-to-one relationships using Dapper

---
+++ DataAccess/Dapper/Dapper-10/Dapper-10/Models/CareerItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dapper_10.Models
+{
+    class CareerItem
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public Course Course { get; set; }
+    }
+}
---
+++ DataAccess/Dapper/Dapper-10/Dapper-10/Program.cs
@@ -0,0 +1,44 @@
+﻿using Dapper;
+using Dapper_10.Models;
+using Microsoft.Data.SqlClient;
+using System;
+
+namespace Dapper_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string connectionString = "Data Source=localhost,1433; Database=balta; User ID=sa; Password=SenhaDoSA123; Encrypt=False";
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                OneToOne(connection);
+            }
+        }
+
+        static void OneToOne(SqlConnection connection)
+        {
+            var sql = @"
+                SELECT 
+                    * 
+                FROM 
+                    [CareerItem] 
+                INNER JOIN 
+                    [Course] ON [CareerItem].[CourseId] = [Course].[Id]";
+
+            var items = connection.Query<CareerItem, Course, CareerItem>(
+                sql,
+                (careerItem, course) =>
+                {
+                    careerItem.Course = course;
+                    return careerItem;
+                }, splitOn: "Id");
+
+            foreach (var item in items)
+            {
+                Console.WriteLine($"{item.Title} - Course: {item.Course.Title}");
+            }
+        }
+    }
+}
