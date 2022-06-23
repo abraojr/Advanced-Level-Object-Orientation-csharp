9a85a13a0b95db0a5c50d65dfcb6d7d92a37f077
Abrão Astério Junior <abraoastjr@gmail.com>
2022-06-23 11:11:37 -0300

    Execute Scalar using Dapper

---
+++ DataAccess/Dapper/Dapper-08/Dapper-08/Program.cs
@@ -0,0 +1,56 @@
+﻿using Dapper;
+using Dapper_08.Models;
+using Microsoft.Data.SqlClient;
+using System;
+
+namespace Dapper_08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string connectionString = "Data Source=localhost,1433; Database=balta; User ID=sa; Password=SenhaDoSA123; Encrypt=False";
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                ExecuteScalar(connection);
+            }
+        }
+
+        static void ExecuteScalar(SqlConnection connection)
+        {
+            var category = new Category();
+            category.Title = "Amazon AWS";
+            category.Url = "amazon";
+            category.Description = "Category for AWS Services";
+            category.Order = 8;
+            category.Summary = "AWS Cloud";
+            category.Featured = false;
+
+            var insertSql = @"
+                INSERT INTO 
+                    [Category] 
+                OUTPUT inserted.[Id]
+                VALUES(
+                    NEWID(), 
+                    @Title, 
+                    @Url, 
+                    @Summary, 
+                    @Order, 
+                    @Description, 
+                    @Featured) 
+                ";
+
+            var id = connection.ExecuteScalar<Guid>(insertSql, new
+            {
+                category.Title,
+                category.Url,
+                category.Summary,
+                category.Order,
+                category.Description,
+                category.Featured
+            });
+            Console.WriteLine($"The category inserted was: {id}");
+        }
+    }
+}
