c855bd7413d99188e667124b648e4acfddb4fcff
Abrão Astério Junior <abraoastjr@gmail.com>
2022-06-23 12:25:17 -0300

    QueryMultiple using Dapper

---
+++ DataAccess/Dapper/Dapper-12/Dapper-12/Models/Course.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dapper_12.Models
+{
+    class Course
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+    }
+}
---
+++ DataAccess/Dapper/Dapper-12/Dapper-12/Program.cs
@@ -0,0 +1,40 @@
+﻿using Dapper;
+using Dapper_12.Models;
+using Microsoft.Data.SqlClient;
+using System;
+
+namespace Dapper_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string connectionString = "Data Source=localhost,1433; Database=balta; User ID=sa; Password=SenhaDoSA123; Encrypt=False";
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                QueryMutiple(connection);
+            }
+        }
+        static void QueryMutiple(SqlConnection connection)
+        {
+            var query = "SELECT * FROM [Category]; SELECT * FROM [Course]";
+
+            using (var multi = connection.QueryMultiple(query))
+            {
+                var categories = multi.Read<Category>();
+                var courses = multi.Read<Course>();
+
+                foreach (var item in categories)
+                {
+                    Console.WriteLine(item.Title);
+                }
+
+                foreach (var item in courses)
+                {
+                    Console.WriteLine(item.Title);
+                }
+            }
+        }
+    }
+}
