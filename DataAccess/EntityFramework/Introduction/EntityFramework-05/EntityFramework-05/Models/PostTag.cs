b9605fe57e761453e35f81b30da07f649a3492bc
Abrão Astério Junior <abraoastjr@gmail.com>
2022-06-27 14:25:04 -0300

    Using AsNoTracking

---
+++ DataAccess/EntityFramework/Introduction/EntityFramework-05/EntityFramework-05/Models/PostTag.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EntityFramework_05.Models
+{
+    public class PostTag
+    {
+        public int PostId { get; set; }
+        public int TagId { get; set; }
+    }
+}
---
+++ DataAccess/EntityFramework/Introduction/EntityFramework-05/EntityFramework-05/Program.cs
@@ -0,0 +1,23 @@
+﻿using EntityFramework_05.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+
+namespace EntityFramework_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new BlogDataContext())
+            {
+                var tags = context.Tags.AsNoTracking().ToList();
+
+                foreach (var tag in tags)
+                {
+                    Console.WriteLine(tag.Name);
+                }
+            }
+        }
+    }
+}
