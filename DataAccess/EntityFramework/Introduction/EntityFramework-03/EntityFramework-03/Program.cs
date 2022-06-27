e02396a977fbdefe4992b80379feb1ecf0087898
Abrão Astério Junior <abraoastjr@gmail.com>
2022-06-27 14:22:18 -0300

    Delete operation using EF

---
+++ DataAccess/EntityFramework/Introduction/EntityFramework-03/EntityFramework-03/Program.cs
@@ -0,0 +1,20 @@
+﻿using EntityFramework_03.Data;
+using System;
+using System.Linq;
+
+namespace EntityFramework_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new BlogDataContext())
+            {
+                var tag = context.Tags.FirstOrDefault(x => x.Id == 2);
+
+                context.Remove(tag);
+                context.SaveChanges();
+            }
+        }
+    }
+}
