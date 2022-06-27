a0948ae1f0d9d2263910252f728ea0d70d240d65
Abrão Astério Junior <abraoastjr@gmail.com>
2022-06-27 14:18:40 -0300

    Setting up the DbSet, DataContext and Create operation

---
+++ DataAccess/EntityFramework/Introduction/EntityFramework-01/EntityFramework-01/Program.cs
@@ -0,0 +1,19 @@
+﻿using EntityFramework_01.Data;
+using EntityFramework_01.Models;
+using System;
+
+namespace EntityFramework_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new BlogDataContext())
+            {
+                var tag = new Tag { Name = "ASP.NET", Slug = "aspnet" };
+                context.Tags.Add(tag);
+                context.SaveChanges();
+            }
+        }
+    }
+}
