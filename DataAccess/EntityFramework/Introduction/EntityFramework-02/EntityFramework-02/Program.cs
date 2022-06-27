e8d94b14c945172a5796fee73bf117d07ab17aa1
Abrão Astério Junior <abraoastjr@gmail.com>
2022-06-27 14:21:17 -0300

    Update operation using EF

---
+++ DataAccess/EntityFramework/Introduction/EntityFramework-02/EntityFramework-02/Program.cs
@@ -0,0 +1,22 @@
+﻿using EntityFramework_02.Data;
+using System;
+using System.Linq;
+
+namespace EntityFramework_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new BlogDataContext())
+            {
+                var tag = context.Tags.FirstOrDefault(x => x.Id == 2);
+                tag.Name = ".NET";
+                tag.Slug = "dotnet";
+
+                context.Update(tag);
+                context.SaveChanges();
+            }
+        }
+    }
+}
