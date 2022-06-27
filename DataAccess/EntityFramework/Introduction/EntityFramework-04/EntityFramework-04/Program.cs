44d8497d5182675e9525e17fa680448f3cc4cb70
Abrão Astério Junior <abraoastjr@gmail.com>
2022-06-27 14:24:15 -0300

    Selection operation with ToList using EF

---
+++ DataAccess/EntityFramework/Introduction/EntityFramework-04/EntityFramework-04/Program.cs
@@ -0,0 +1,22 @@
+﻿using EntityFramework_04.Data;
+using System;
+using System.Linq;
+
+namespace EntityFramework_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new BlogDataContext())
+            {
+                var tags = context.Tags.ToList();
+
+                foreach (var tag in tags)
+                {
+                    Console.WriteLine(tag.Name);
+                }
+            }
+        }
+    }
+}
