21c9a60c1371b11ffbe6830a34cb8c9128186afa
Abrão Astério Junior <abraoastjr@gmail.com>
2022-06-27 18:35:09 -0300

    Data Annotations and Navigation Properties

---
+++ DataAccess/EntityFramework/Basic-Operations/EntityFramework-01/EntityFramework-01/Data/BlogDataContext.cs
@@ -0,0 +1,24 @@
+﻿using EntityFramework_01.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFramework_01.Data
+{
+    public class BlogDataContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        //public DbSet<PostTag> PostTags { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<User> Users { get; set; }
+        //public DbSet<UserRole> UserRoles { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options) => options.UseSqlServer("Data Source=localhost,1433; Database=Blog; User ID=sa; Password=SenhaDoSA123; Encrypt=False");
+
+    }
+}
