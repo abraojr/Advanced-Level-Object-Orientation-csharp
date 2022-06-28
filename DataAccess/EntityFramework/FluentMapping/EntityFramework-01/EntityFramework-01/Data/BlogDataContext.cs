95bc1b208c1bc479673a85ad16a9c7265ff05527
Abrão Astério Junior <abraoastjr@gmail.com>
2022-06-28 14:00:13 -0300

    Testing the created mappings

--- DataAccess/EntityFramework/FluentMapping/EntityFramework-01/EntityFramework-01/Data/BlogDataContext.cs
+++ DataAccess/EntityFramework/FluentMapping/EntityFramework-01/EntityFramework-01/Data/BlogDataContext.cs
@@ -1,4 +1,5 @@
-﻿using EntityFramework_01.Models;
+﻿using EntityFramework_01.Data.Mappings;
+using EntityFramework_01.Models;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -10,6 +11,17 @@ namespace EntityFramework_01.Data
 {
     public class BlogDataContext : DbContext
     {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<User> Users { get; set; }
+
         protected override void OnConfiguring(DbContextOptionsBuilder options) => options.UseSqlServer("Data Source=localhost,1433; Database=Blog; User ID=sa; Password=SenhaDoSA123; Encrypt=False");
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new CategoryMap());
+            modelBuilder.ApplyConfiguration(new UserMap());
+            modelBuilder.ApplyConfiguration(new PostMap());
+        }
     }
 }
--- DataAccess/EntityFramework/FluentMapping/EntityFramework-01/EntityFramework-01/Program.cs
+++ DataAccess/EntityFramework/FluentMapping/EntityFramework-01/EntityFramework-01/Program.cs
@@ -1,5 +1,7 @@
 ﻿using EntityFramework_01.Data;
+using EntityFramework_01.Models;
 using System;
+using System.Linq;
 
 namespace EntityFramework_01
 {
@@ -8,6 +10,37 @@ namespace EntityFramework_01
         static void Main(string[] args)
         {
             using var context = new BlogDataContext();
+
+            // Adding a User
+            context.Users.Add(new User
+            {
+                Bio = "UserTest",
+                Email = "test@email.com",
+                Image = "htps://...",
+                Name = "User Test",
+                PasswordHash = "1234",
+                Slug = "user-test"
+            });
+            context.SaveChanges();
+
+            // Adding a post
+            var user = context.Users.FirstOrDefault(x => x.Id == 4);
+            var post = new Post
+            {
+                Author = user,
+                Body = "My Article",
+                Category = new Category
+                {
+                    Name = "Frontend",
+                    Slug = "frontend"
+                },
+                CreateDate = DateTime.Now,
+                Slug = "my-article",
+                Summary = "In this summary we will see...",
+                Title = "My Article"
+            };
+            context.Posts.Add(post);
+            context.SaveChanges();
         }
     }
 }
