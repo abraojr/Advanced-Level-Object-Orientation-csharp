42f08f613ad55434f1842e4881396f66756492c3
Abrão Astério Junior <abraoastjr@gmail.com>
2022-06-28 10:17:25 -0300

    Using Include in EF Core

---
+++ DataAccess/EntityFramework/Basic-Operations/EntityFramework-03/EntityFramework-03/Program.cs
@@ -0,0 +1,27 @@
+﻿using EntityFramework_03.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+
+namespace EntityFramework_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using var context = new BlogDataContext();
+
+            var posts = context
+                .Posts
+                .AsNoTracking()
+                .Include(x => x.Author)
+                .OrderBy(x => x.LastUpdateDate)
+                .ToList();
+
+            foreach (var post in posts)
+            {
+                Console.WriteLine($"{post.Title} written by {post.Author?.Name}");
+            }
+        }
+    }
+}
