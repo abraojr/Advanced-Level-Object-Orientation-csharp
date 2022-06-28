37166687210110f26846f8529163906d3bce15b2
Abrão Astério Junior <abraoastjr@gmail.com>
2022-06-28 18:32:42 -0300

    Creating Migration

---
+++ DataAccess/EntityFramework/FluentMapping/EntityFramework-02/EntityFramework-02/Data/Mappings/PostMap.cs
@@ -0,0 +1,75 @@
+﻿using EntityFramework_02.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFramework_02.Data.Mappings
+{
+    public class PostMap : IEntityTypeConfiguration<Post>
+    {
+        public void Configure(EntityTypeBuilder<Post> builder)
+        {
+            // Table
+            builder.ToTable("Post");
+
+            // Primary Key
+            builder.HasKey(x => x.Id);
+
+            // Identity
+            builder.Property(x => x.Id)
+                .ValueGeneratedOnAdd()
+                .UseIdentityColumn();
+
+            // Properties
+            builder.Property(x => x.LastUpdateDate)
+                .IsRequired()
+                .HasColumnName("LastUpdateDate")
+                .HasColumnType("SMALLDATETIME")
+                .HasMaxLength(60)
+                .HasDefaultValueSql("GETDATE()");
+            //.HasDefaultValue(DateTime.Now.ToUniversalTime())
+
+
+
+            builder.Property(x => x.Slug)
+                .IsRequired()
+                .HasColumnName("Slug")
+                .HasColumnType("VARCHAR")
+                .HasMaxLength(80);
+
+            // Indexes
+            builder.HasIndex(x => x.Slug, "IX_Post_Slug")
+                .IsUnique();
+
+            // Relationships
+            builder.HasOne(x => x.Author)
+                .WithMany(x => x.Posts)
+                .HasConstraintName("FK_Post_Author")
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasOne(x => x.Category)
+               .WithMany(x => x.Posts)
+               .HasConstraintName("FK_Post_Category")
+               .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasMany(x => x.Tags)
+                .WithMany(x => x.Posts)
+                .UsingEntity<Dictionary<string, object>>(
+                    "PostTag",
+                    post => post.HasOne<Tag>()
+                        .WithMany()
+                        .HasForeignKey("PostId")
+                        .HasConstraintName("FK_PostTag_PostId")
+                        .OnDelete(DeleteBehavior.Cascade),
+                    tag => tag.HasOne<Post>()
+                        .WithMany()
+                        .HasForeignKey("TagId")
+                        .HasConstraintName("FK_PostTag_TagId")
+                        .OnDelete(DeleteBehavior.Cascade));
+        }
+    }
+}
